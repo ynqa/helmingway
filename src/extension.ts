842b9495c12cc677e8e440451a18fad07f9368b1
ynqa <un.pensiero.vano@gmail.com>
2026-04-20 21:01:53 +0900

    sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,25 @@
 import * as vscode from "vscode";
 
-export function activate(_context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
   console.log("Helmingway extension is now active.");
+
+  const provider = new HelmingwayPreviewProvider();
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider("helmingway.preview", provider),
+  );
 }
 
 export function deactivate() {}
+
+/**
+ * Provides Helmingway sidebar tree shown in VS Code Side View.
+ */
+class HelmingwayPreviewProvider implements vscode.TreeDataProvider<never> {
+  getTreeItem(element: never): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(): never[] {
+    return [];
+  }
+}
