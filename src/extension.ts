3125ae076fb73ad56b236c65f8cabe77ea943f1b
ynqa <un.pensiero.vano@gmail.com>
2026-04-20 20:26:24 +0900

    step1

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from "vscode";
+
+export function activate(_context: vscode.ExtensionContext) {
+  console.log("Helmingway extension is now active.");
+}
+
+export function deactivate() {}
